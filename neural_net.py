63ffd4819f15e9690e59b354b62043ec3b2d6d09
kiankd <kian.kenyon-dean@mail.mcgill.ca>
2015-10-31 17:02:45 -0400

    Learning git slowly...

--- neural_net.py
+++ neural_net.py
@@ -3,11 +3,23 @@
 
 """ The main neural network classifier object and its methods are here. """
 
-class NeuralNetwork:
-    def __init__(self):
-        self.x = 0
+class NeuralNetworkClassifier:
+    """ Our neural network that perform classification. """
+    def __init__(self, num_hidden_layers, hidden_nodes_per_layer):
+        self.num_hidden_layers = num_hidden_layers
+        self.hidden_nodes_per_layer = hidden_nodes_per_layer
+
+
+    def fit(self, X, y):
+        return self.num_hidden_layers
+
+class InputNode:
+    """ All training features will each have a single input node. """
+    def __init__(self, value):
+        self.value = value
 
 class Neuron:
+    """ A neuron. """
     def __init__(self):
         self.x = 0
 
